1b74d9471a31e4ad847ec56b8d4af7b2c62c0ce7
David You <dyou3968@gmail.com>
2020-04-04 11:40:46 -0400

    Update weatherFunction.py

--- Hack112/weatherFunction.py
+++ Hack112/weatherFunction.py
@@ -12,8 +12,6 @@ NOTE:
 # sentences 
 """
 
-#pip install openweathermap.org
-
 # Round Half Up Import
 import decimal
 
@@ -23,41 +21,70 @@ import urllib
 from urllib import request
 
 
-# Taken https://www.cs.cmu.edu/~112/
-def roundHalfUp(d):
-    # Round to nearest with ties going away from zero.
-    rounding = decimal.ROUND_HALF_UP
-    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
+class WeatherAnswers(object):
+    # Taken https://www.cs.cmu.edu/~112/
+    def __init__(self):
+        return
+    def roundHalfUp(self, d):
+        # Round to nearest with ties going away from zero.
+        rounding = decimal.ROUND_HALF_UP
+        return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
 
-def weatherToString(text):
-    sample = text.split(" ")
-    index = sample.index("in")
-    city = sample[index+1:]
-    city = "+".join(city)
-    # Openweathermap is the API we used to gather weather data. 
-    # This site gave us information on how to access our weather data:
-    # https://openweathermap.org/current
-    my_url = f'''http://api.openweathermap.org/data/2.5/weather?q={city}&appid=fea4bf62b107b32ea76781885181c0a0'''
-    # TA Ping-Ya helped us with converting the text in my_url to a workable type
-    # ig the next two lines 
-    htmlText = urllib.request.urlopen(my_url)
-    contents = htmlText.readlines()
-    contents = contents[0]
-    # The next line to convert from bytes to dictionaries came from this site:
-    # https://www.geeksforgeeks.org/python-interconversion-between-dictionary-and-bytes/
-    dictionaryContents = json.loads(contents.decode('utf-8'))
-    return dictionaryContents
+    def weatherToString(self, text):
+        sample = text.split(" ")
+        index = sample.index("in")
+        city = sample[index+1:]
+        city = "+".join(city)
+        # Openweathermap is the API we used to gather weather data. 
+        # This site gave us information on how to access our weather data:
+        # https://openweathermap.org/current
+        my_url = f'''http://api.openweathermap.org/data/2.5/weather?q={city}&appid=fea4bf62b107b32ea76781885181c0a0'''
+        # TA Ping-Ya helped us with converting the text in my_url to a workable type
+        # ig the next two lines 
+        htmlText = urllib.request.urlopen(my_url)
+        contents = htmlText.readlines()
+        contents = contents[0]
+        # The next line to convert from bytes to dictionaries came from this site:
+        # https://www.geeksforgeeks.org/python-interconversion-between-dictionary-and-bytes/
+        dictionaryContents = json.loads(contents.decode('utf-8'))
+        return dictionaryContents
 
-def kelvinToFarenheit(kelvin):
-    return 32+9/5*(kelvin-273)
+    def kelvinToFarenheit(self, kelvin):
+        return 32+9/5*(kelvin-273)
+    
+    def inRange(self, number, two):
+        if (number >= two[0]) and (number <= two[1]): return True
+        else: return False
 
-def DAVIDweather(text):
-    report = weatherToString(text)
-    weather = report["weather"][0]
-    main = report["main"]
-    temperature = roundHalfUp(kelvinToFarenheit((main['feels_like'])))
-    city = report['name']
-    weatherString = (f"Right now, the weather in {city} consists of {weather['description']}." + 
-                    f' It feels like {temperature} degrees Fahrenheit.')
-    return weatherString
+    def getWind(self, windData):
+        speed = windData['speed']
+        angle = windData['deg']
+        direction = ["north", "east", "south", "west"][self.roundHalfUp(angle/90)%4]
+        windNames = [[[8, 12],"gentle breeze"], [[13, 18],"moderate breeze"], [[19, 24],"fresh breeze"],
+                     [[25, 31],"strong breeze"], [[32, 46],"gale"], [[47,63],"storm"], [[64,80],"violent storm"]]
+        if speed < 8: return ""
+        else:
+            for i in windNames:
+                if inRange(speed, i[0]):
+                    return f" It's windy today, with a {i[1]} facing {direction}"
+            return " Don't venture outside. Find a safe place and hunker down."
+
+    def DAVIDweather(self,text):
+        report = self.weatherToString(text)
+        weather = report["weather"][0]
+        main = report["main"]
+        temperature = self.roundHalfUp(self.kelvinToFarenheit((main['temp'])))
+        feelsLike = self.roundHalfUp(self.kelvinToFarenheit((main['feels_like'])))
+        city = report['name']
+        windInsert = self.getWind(report['wind'])
+        if abs(feelsLike - temperature) < 5:
+            weatherString = (f"Right now, the weather in {city} consists of {weather['description']}." + 
+                             f' The temperature outside is {temperature} degrees.{windinsert}')
+        else:
+            weatherString = (f"Right now, the weather in {city} consists of {weather['description']}." + 
+                             f' The temperature outside is {temperature} degrees, but it feels like {feelsLike}.{windinsert}')
+        return weatherString
+
+
+
 
