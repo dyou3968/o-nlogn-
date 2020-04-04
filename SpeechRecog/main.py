af4fd42a91cd640b1153372bf113331a3e93943f
David You <dyou3968@gmail.com>
2020-04-04 12:37:05 -0400

    Update main.py

--- SpeechRecog/main.py
+++ SpeechRecog/main.py
@@ -8,14 +8,16 @@ from googleTranslateFunction import *
 
 
 def process(text):
+    text = text.lower()
     words = text.split(" ")
-    keyWeatherWords = []
-    keyGoogleTranslateWords = []
-    print(text)
-    if "translate" in words:
-        trans = Translate()
-        return trans.DAVIDtranslator(text)
-    if "weather" in words:
+    if "homework" in words or "code" in words or "112" in words:
+        return (f"Sorry, I cannot do that. That would be an academic integrity violation." + 
+                f"Please ask the TAs for help. If you go any further down this path"
+                f"I will have to get the real David Kosbie")
+    if "language" in words or "translate" in words:
+        translate = Translate()
+        return translate.DAVIDtranslator(text)
+    if "weather" in words or "temperature" in words:
         wf = WeatherAnswers()
         return wf.DAVIDweather(text)
     else:
