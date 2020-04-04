6ecc89c0ccb408b3b12a688e0849e17d83ca2583
SeanSwayze <seanswayze1@gmail.com>
2020-04-04 10:55:05 -0400

    Update googleTranslateFunction.py

--- SpeechRecog/googleTranslateFunction.py
+++ SpeechRecog/googleTranslateFunction.py
@@ -1,3 +1,5 @@
+#pip install googletrans
+
 import googletrans
 from googletrans import Translator
 
@@ -23,3 +25,19 @@ class Translate(object):
         result = translator.translate(words, dest = language)
         answer = result.text
         return answer
+
+def testDAVIDtranslator():
+    print('Testing DAVIDtranslator()...', end='')
+    sample1 = "D.A.V.I.D., translate I am asleep into Spanish"
+    assert(DAVIDtranslator(sample1) == "Estoy dormido")
+    sample2 = "D.A.V.I.D., translate I am asleep into Spanish"
+    assert(DAVIDtranslator(sample2) == "Estoy dormido")
+    sample3 = "D.A.V.I.D., translate That is good into German"
+    assert(DAVIDtranslator(sample3) == "Das ist gut")
+    sample4 = "D.A.V.I.D., translate Das ist gut"
+    assert(DAVIDtranslator(sample4) == "It's good")
+    sample5 = "D.A.V.I.D., translate I don't know into Chinese"
+    assert(DAVIDtranslator(sample5) == "我不知道")
+    sample6 = "D.A.V.I.D., translate 我不知道 into English"
+    assert(DAVIDtranslator(sample6) == "I do not know")
+    print('Passed')
