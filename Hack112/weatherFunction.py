3547f9546288ccce3e5e1d3767419b587edb650d
David You <dyou3968@gmail.com>
2020-04-04 12:21:31 -0400

    Update weatherFunction.py

--- Hack112/weatherFunction.py
+++ Hack112/weatherFunction.py
@@ -56,35 +56,57 @@ class WeatherAnswers(object):
         if (number >= two[0]) and (number <= two[1]): return True
         else: return False
 
-    def getWind(self, windData):
+    def getWind(self, report, solo):
+        windData = report['wind']
         speed = windData['speed']
         angle = windData['deg']
+        city = report['name']
         direction = ["north", "east", "south", "west"][self.roundHalfUp(angle/90)%4]
         windNames = [[[8, 12],"gentle breeze"], [[13, 18],"moderate breeze"], [[19, 24],"fresh breeze"],
                      [[25, 31],"strong breeze"], [[32, 46],"gale"], [[47,63],"storm"], [[64,80],"violent storm"]]
-        if speed < 8: return ""
+        if (speed < 8) and (solo == False): return ""
+        elif (speed < 8) and (solo == True):
+            return f"It's a calm day in {city}, with a light breeze of {speed} miles per hour moving {direction}."
         else:
             for i in windNames:
                 if inRange(speed, i[0]):
-                    return f" It's windy today, with a {i[1]} facing {direction}"
-            return " Don't venture outside. Find a safe place and hunker down."
+                    return f"It's windy today, with a {i[1]} facing {direction}."
+            return "Don't venture outside. Find a safe place and hunker down."
 
-    def DAVIDweather(self,text):
-        report = self.weatherToString(text)
+    def getWeather(self, report):
         weather = report["weather"][0]
         main = report["main"]
         temperature = self.roundHalfUp(self.kelvinToFarenheit((main['temp'])))
         feelsLike = self.roundHalfUp(self.kelvinToFarenheit((main['feels_like'])))
         city = report['name']
-        windInsert = self.getWind(report['wind'])
+        windInsert = self.getWind(report, False)
         if abs(feelsLike - temperature) < 5:
             weatherString = (f"Right now, the weather in {city} consists of {weather['description']}." + 
-                             f' The temperature outside is {temperature} degrees.{windInsert}')
+                             f' The temperature outside is {temperature} degrees. {windTnsert}')
         else:
             weatherString = (f"Right now, the weather in {city} consists of {weather['description']}." + 
-                             f' The temperature outside is {temperature} degrees, but it feels like {feelsLike}.{windInsert}')
+                             f' The temperature outside is {temperature} degrees, but it feels like {feelsLike}. {windTnsert}')
         return weatherString
 
+    def getHumidity(self, report):
+        main = report['main']
+        city = report['name']
+        humidity = main['humidity']
+        return f"The humidity in {city} is currently {humidity} percent."
 
+    def getTemperature(self, report):
+        main = report['main']
+        city = report['name']
+        temp = self.roundHalfUp(self.kelvinToFarenheitmain(main['temp']))
+        return f"The temperature in {city} is currently {temp} degrees."
 
-
+    def DAVIDweather(self,text):
+        report = self.weatherToString(text)
+        if "weather" in text:
+            return self.getWeather(report)
+        if "wind speed" in text:
+            return self.getWind(report, True)
+        if "humidity" in text:
+            return self.getHumidity(report)
+        if "temperature" in text:
+            return self.getTemperature(report)
