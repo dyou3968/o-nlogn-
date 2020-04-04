ddab89b4fe2746d1b4c0c3404bb87315f9adb11e
Matthew <matthewlucienkomar@gmail.com>
2020-04-03 23:37:57 -0400

    LMK 4.3.20 SPEECH Reliably Works
    Speech system reliably works. It exports to Speechfile.txt.
    It uses the syntax:
    CommandA = Cat Hat
    CommandB = TT Yeet
    Therefore, it would be Cat Hat_TT Yeet
    I.e.:
    testing testing_you working_David_Matthew_Jake_monitor_cat_Narnia_stop_

--- SpeechRecog/SpeechText.py
+++ SpeechRecog/SpeechText.py
@@ -5,10 +5,21 @@
 import speech_recognition as sr
 #sr.__version__
 r = sr.Recognizer()
-with sr.Microphone() as source:                # use the default microphone as the audio source
-    audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
-    print("Running")
-try:
-    print("You said " + r.recognize_google(audio))    # recognize speech using Google Speech Recognition
-except LookupError:                            # speech is unintelligible
-    print("Could not understand audio")
+File = open("speechfile.txt","w+")
+on = True
+while on:
+    with sr.Microphone() as source:
+        # use the default microphone as the audio source
+        # listen for the first phrase and extract it into audio data
+        audio = r.listen(source)
+        print("Running")
+    try:
+        audioInput = r.recognize_google(audio)
+        File.write(audioInput+'_')    
+        if (audioInput == "stop"):
+            on = False
+            File.close()
+        # recognize speech using Google Speech Recognition
+    except sr.UnknownValueError:                            
+    #    # speech is unintelligible
+        print("Could not understand audio")
