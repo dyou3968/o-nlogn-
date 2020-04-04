9fc2ec44d120a258802b6cf037a0dc130701088f
David You <dyou3968@gmail.com>
2020-04-04 12:06:27 -0400

    SortText Update

--- Hack112/SortText.py
+++ Hack112/SortText.py
@@ -4,13 +4,14 @@ from googleTranslateFunction import *
 
 
 def process(text):
+    text = text.lower()
     words = text.split(" ")
-    keyWeatherWords = []
-    keyGoogleTranslateWords = []
-    if "translate" in words:
-        return DAVIDtranslator(text)
-    if "weather" in words:
-        return DAVIDweather(text)
+    if "language" in words or "translate" in words:
+        translate = Translate()
+        return translate.DAVIDtranslator(text)
+    if "weather" in words or "temperature" in words:
+        wf = WeatherAnswers()
+        return wf.DAVIDweather(text)
     else:
         return DAVIDwolframalpha(text)
 
@@ -18,5 +19,3 @@ text = input("Input: ").lower()
 while text != "break":
     print(process(text))
     text = input("Input: ").lower()
-
-        
