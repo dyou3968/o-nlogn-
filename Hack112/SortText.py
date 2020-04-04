a1d100eb019077b2bb5a5cecc7b9e5bea83d019b
David You <dyou3968@gmail.com>
2020-04-04 12:20:05 -0400

    SortText Update

--- Hack112/SortText.py
+++ Hack112/SortText.py
@@ -6,6 +6,10 @@ from googleTranslateFunction import *
 def process(text):
     text = text.lower()
     words = text.split(" ")
+    if "homework" in words or "code" in words or "112" in words:
+        return (f"Sorry, I cannot do that. That would be an academic integrity violation." + 
+                f"Please ask the TAs for help. If you go any further down this path"
+                f"I will have to get the real David Kosbie")
     if "language" in words or "translate" in words:
         translate = Translate()
         return translate.DAVIDtranslator(text)
--- Hack112/textToSpeech.py
+++ Hack112/textToSpeech.py
@@ -3,6 +3,8 @@ import pyttsx3
 # Taken from https://pypi.org/project/pyttsx3/
 engine = pyttsx3.init()
 #text='DAVID what is the weather like in Pittsburgh'
-text = "Estoy haciendo mi proyecto"
+text = (f"Sorry, I cannot do that. That would be an academic integrity violation." + 
+        f"Please ask the TAs for help. If you go any further down this path"
+        f"I will have to get the real David Kosbie")
 engine.say(text)
 engine.runAndWait()
