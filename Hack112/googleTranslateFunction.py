93ae2cf0b06664b8f1b60f526a846a8d1c8040f9
David You <dyou3968@gmail.com>
2020-04-04 10:52:49 -0400

    Update googleTranslateFunction.py

--- Hack112/googleTranslateFunction.py
+++ Hack112/googleTranslateFunction.py
@@ -3,24 +3,28 @@
 import googletrans
 from googletrans import Translator
 
-def DAVIDtranslator(text):
-    # Takes in a same string and returns the message
-    # In the language of the users choice
-    words = text.split(" ")
-    start = words.index("translate")
-    indexWords = ["in", "to", "into"]
-    language = None
-    for word in indexWords:
-        if words[-2] == word:
-            language = words[-1]
-    if language == None: language = "english"
-    if language == "Chinese":
-        language = "chinese (simplified)"
-    words = " ".join(words[start+1:-2])
-    translator = Translator()
-    result = translator.translate(words, dest = language)
-    answer = result.text
-    return answer
+class Translate(object):
+    def DAVIDtranslator(self, text):
+        # Takes in a same string and returns the message
+        # In the language of the users choice
+        words = text.split(" ")
+        start = words.index("translate")
+        indexWords = ["in", "to", "into"]
+        language = None
+        for word in indexWords:
+            if words[-2] == word:
+                language = words[-1]
+                if language == "Chinese":
+                    language = "chinese (simplified)"
+                words = " ".join(words[start+1:-2])
+                break
+        if language == None: 
+            language = "english"
+            words = " ".join(words[start+1:])
+        translator = Translator()
+        result = translator.translate(words, dest = language)
+        answer = result.text
+        return answer
 
 def testDAVIDtranslator():
     print('Testing DAVIDtranslator()...', end='')
