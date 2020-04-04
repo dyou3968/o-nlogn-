aa4819a7ce09926a4f17d4bf5d783f49928dbed6
David You <dyou3968@gmail.com>
2020-04-04 13:18:00 -0400

    Update main.py

--- SpeechRecog/main.py
+++ SpeechRecog/main.py
@@ -8,18 +8,27 @@ from googleTranslateFunction import *
 def process(text):
     text = text.lower()
     words = text.split(" ")
-    if "homework" in words or "code" in words or "112" in words:
+    if ("homework" in words) or ("code" in words) or ("112" in words):
         return (f"Sorry, I cannot do that. That would be an academic integrity violation." + 
                 f"Please ask the TAs for help. If you go any further down this path"
                 f"I will have to get the real David Kosbie")
-    if "language" in words or "translate" in words:
+    if ("language" in words) or ("translate" in words):
         translate = Translate()
         return translate.DAVIDtranslator(text)
-    if "weather" in words or "temperature" in words:
+    if ("weather" in words) or ("temperature" in words) or ("wind speed" in words) or ("humidity" in words):
         wf = WeatherAnswers()
         return wf.DAVIDweather(text)
     else:
         return DAVIDwolframalpha(text)
+
+def audioOutput(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    return
+
+text = "What is the weather like in Pittsburgh"
+audioOutput(text)
 
 r = sr.Recognizer()
 File = open("speechfile.txt","w+")
@@ -43,10 +52,3 @@ while on:
     except sr.UnknownValueError:                            
     #    # speech is unintelligible
         print("Could not understand audio")
-
-
-def audioOutput(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-    return
