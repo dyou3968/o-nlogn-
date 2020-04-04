ac323f7de3283597c59cacae77a54736c9908a5e
Matthew <matthewlucienkomar@gmail.com>
2020-04-04 15:45:53 -0400

    Grammatical Fix

--- SpeechRecog/todoListFunction.py
+++ SpeechRecog/todoListFunction.py
@@ -62,10 +62,10 @@ def removeTask(path, task):
 
 def readTask(path):
     entries = getTodoListData(path)
-    if len(entries) = 0:
+    if len(entries) == 0:
         return "There is nothing on your to do list"
     else:
-        message = "Today on your to do list"
+        message = "Today on your to do list: "
         entries = ". ".join(entries)
         return message + entries
 
